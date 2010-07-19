e0c138d08096bfbd0b5379381251d6a63d43b011
fversnel <devnull@localhost>
2010-07-19 17:36:32 +0000

    Set up the testing framework.

---
+++ src/org/klaverjasaichallenge/score/test/TestScore.java
@@ -0,0 +1,20 @@
+package org.klaverjasaichallenge.score.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import org.klaverjasaichallenge.score.Score;
+
+
+public class TestScore {
+	
+	/**
+	 * TODO Implement this test.
+	 */
+	@Test
+	public void testRoem() {
+		assert(true);
+	}	
+
+}
