d721b6c3f122916a4d4ea75c35e361fe7c8f5678
fversnel <devnull@localhost>
2010-07-22 18:54:14 +0000

    Added functionality for determining the leading suit in a trick.

--- src/org/klaverjasaichallenge/shared/Trick.java
+++ src/org/klaverjasaichallenge/shared/Trick.java
@@ -5,23 +5,33 @@ import java.util.List;
 import java.util.LinkedList;
 
 import org.klaverjasaichallenge.shared.card.Card;
+import org.klaverjasaichallenge.shared.card.suit.Suit;
 
 public class Trick {
+	private static final int FIRST_ADDED_CARD = 1;
+
 	private List<Card> cards;
+	private Suit leadingSuit;
 
 	public Trick() {
 		this.cards = new LinkedList<Card>();
-	}
-
-	public Trick(List<Card> cards) {
-		this.cards = cards;
+		this.leadingSuit = null;
 	}
 
 	public void addCard(Card card) {
 		this.cards.add(card);
+
+		if(this.cards.size() == FIRST_ADDED_CARD) {
+			this.leadingSuit = card.getSuit();
+		}
 	}
 
 	public List<Card> getCards() {
 		return this.cards;
 	}
+
+	public Suit getLeadingSuit() {
+		return this.leadingSuit;
+	}
+
 }
