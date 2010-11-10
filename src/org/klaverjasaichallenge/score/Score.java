74b95073db022e698aca64e28e9afd8ab376805a
f.versnel <devnull@localhost>
2010-11-10 15:07:39 +0000

    Added an extra constructor for initializing a Score object starting at 0 Points.

--- src/org/klaverjasaichallenge/score/Score.java
+++ src/org/klaverjasaichallenge/score/Score.java
@@ -7,10 +7,15 @@ import org.klaverjasaichallenge.shared.card.suit.Suit;
 public class Score {
 	private final Points stockScore;
 	private final Points roemScore;
-	
+
 	public final static Points MARCH_POINTS = new Points(100);
 	public final static Points LAST_TRICK_POINTS = new Points(10);
-	
+
+	public Score() {
+		this.stockScore = new Points();
+		this.roemScore = new Points();
+	}
+
 	public Score(Points stockScore, Points roemScore) {
 		this.stockScore = stockScore;
 		this.roemScore = roemScore;
@@ -31,20 +36,20 @@ public class Score {
 	public Points getStockScore() {
 		return stockScore;
 	}
-	
+
 	public Points getTotalScore() {
 		return Points.plus(getRoemScore(), getStockScore());
 	}
 
-	
+
 	public static Score plus(final Score leftScore, final Score rightScore) {
 		return new Score(Points.plus(leftScore.getStockScore(),rightScore.getStockScore()), Points.plus(leftScore.getRoemScore(),rightScore.getRoemScore()));
 	}
-	
+
 	public static boolean totalScorebiggerThan(final Score leftHandSide, final Score rightHandSide) {
 		return Points.biggerThan(leftHandSide.getTotalScore(), rightHandSide.getTotalScore());
 	}
-	
+
 	public String toString() {
 		return "Score [Total: "+this.getTotalScore()+", Stock: "+ this.getStockScore() +", Roem: " + this.getRoemScore() + "]";
 	}
