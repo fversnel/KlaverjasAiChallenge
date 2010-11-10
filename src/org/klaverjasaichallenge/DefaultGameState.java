4bb24f75cae70c1250312e2b9c3d31c9a54bd736
f.versnel <devnull@localhost>
2010-11-10 15:07:24 +0000

    Added a named constant to represent the amount of games played.

--- src/org/klaverjasaichallenge/DefaultGameState.java
+++ src/org/klaverjasaichallenge/DefaultGameState.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package org.klaverjasaichallenge;
 
@@ -13,19 +13,20 @@ import org.klaverjasaichallenge.shared.Points;
 
 /**
  * Information about the current state of the Game
- * 
+ *
  * Do not return this to the Players, use getPersonalGameState() instead.
- * 
+ *
  * @author Joost Pastoor
- * 
+ *
  */
 public class DefaultGameState {
+	private static final int MAX_ROUNDS = 16;
 
 	private List<Player> players;
 	private List<Round> roundsPlayed;
 
 	public DefaultGameState(List<Player> players) {
-		
+
 		// Put the players in a hashmap that is ordered
 		this.players = players;
 		this.roundsPlayed = new LinkedList<Round>();
@@ -35,42 +36,42 @@ public class DefaultGameState {
 	 * Just play your 16 rounds kk?!
 	 */
 	public void play() {
-		
-		for(int currentRoundId = 0; currentRoundId < 16; currentRoundId++) {
+
+		for(int currentRoundId = 1; currentRoundId <= MAX_ROUNDS; currentRoundId++) {
 			System.out.println("- Starting round: " + currentRoundId + " with Players: " + players);
 
 			Round round = new Round(this.players);
 			round.play();
 			//round.getScores();
-			
+
 			this.roundsPlayed.add(round);
-			
+
 			// Change the order of the players
 			this.players = this.moveOrderUp(this.players);
 		}
 	}
 
 	/**
-	 * 
+	 *
 	 */
 	public Map<Player,Points> getScores() {
 		Map<Player,Points> scores = new HashMap<Player,Points>();
-		
+
 		for(Round round : roundsPlayed) {
-			
+
 		}
-		
+
 		return null;
-	}	
+	}
 
 	private List<Player> moveOrderUp(List<Player> oldList) {
 		List<Player> newList = new ArrayList<Player>();
-		
+
 		newList.add(0,oldList.get(3));
 		newList.add(1,oldList.get(0));
 		newList.add(2,oldList.get(1));
 		newList.add(3,oldList.get(2));
-		
+
 		return newList;
 	}
 }
